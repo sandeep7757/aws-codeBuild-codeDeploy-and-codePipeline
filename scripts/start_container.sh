280a0d16106ebdd6a77329b560120163f8090752
sandeep7757 <84711922+sandeep7757@users.noreply.github.com>
2023-07-20 21:07:27 -0400

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull sandeep7757/simple-python-flask-app
+docker pull sandeep7757/simpleflask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 sandeep7757/simple-python-flask-app
+docker run -d -p 5000:8000 sandeep7757/simple-flask-app
